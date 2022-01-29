65f64999cd6a8e8d5b125d6b25ce4b208de057d2
trif0lium <gh@fleeingsunlight.dev>
2022-01-29 17:52:31 +0700

    feat(internal/backend): NewCacheBackend()

--- internal/backend/backend.go
+++ internal/backend/backend.go
@@ -1,1 +1,20 @@
 package backend
+
+type CacheBackend interface {
+	PutArtifact(hash string, bucketFolder string) error
+	FetchArtifact(hash string, bucketFolder string) error
+}
+
+type cacheBackend struct{}
+
+func NewCacheBackend() CacheBackend {
+	return &cacheBackend{}
+}
+
+func (backend *cacheBackend) PutArtifact(hash string, bucketFolder string) error {
+	return nil
+}
+
+func (backend *cacheBackend) FetchArtifact(hash string, bucketFolder string) error {
+	return nil
+}
