c63a8ddeac1e505fb22bb682ad77f6e085a4d23c
trif0lium <gh@fleeingsunlight.dev>
2022-01-29 23:01:06 +0700

    feat(cmd/function): functions.HTTP()

--- cmd/function/main.go
+++ cmd/function/main.go
@@ -1,1 +1,13 @@
 package main
+
+import (
+	"net/http"
+
+	"github.com/GoogleCloudPlatform/functions-framework-go/functions"
+)
+
+func init() {
+	functions.HTTP("artifacts-api", artifactsAPI)
+}
+
+func artifactsAPI(w http.ResponseWriter, r *http.Request) {}
